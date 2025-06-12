0e25d105b0913876e0437892b7bb8bb9871ee8b2
acph <dragopoot@gmail.com>
2025-06-11 20:40:34 -0600

    Fixing vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/melodijochat/',
-  // other Slidev/Vite configurations
+  slidev: {
+    vue: {
+      /* vue options */
+    },
+    markdown: {
+      /* markdown-it options */
+      markdownItSetup(md) {
+        /* custom markdown-it plugins */
+        md.use(MyPlugin)
+      },
+    },
+    /* options for other plugins */
+  },
 })
